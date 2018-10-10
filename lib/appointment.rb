952ea2669960a1def6a467e78873a86b735a2c37
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-10 17:01:18 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,10 +3,12 @@ require_relative 'doctor'
 
 class Appointment
   @@all = []
+  
   attr_reader :patient, :doctor
   def initialize(patient, doctor, date)
     @patient = patient
     @doctor = doctor
     @date = date
+    @@all << self
   end
 end
