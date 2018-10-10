d34774cf376a795dab3ff4561f507089b6cddfd0
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-10 15:54:08 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,7 @@ class Artist
   def genres
     genres = []
     Song.all.each do |song|
-      genres << song.genre if song.artist.name == self.name
+      genres << song.genre.name if song.artist.name == self.name
     end
     genres
   end
